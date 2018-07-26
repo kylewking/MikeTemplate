773f01efdacf23367684ff6394f1012d32e6f549
Kyle.wang <1191702694@qq.com>
2018-07-26 11:34:16 +0800

    update

--- template/src/store/modules/permission.js
+++ template/src/store/modules/permission.js
@@ -1,28 +1,41 @@
-import { constantRouterMap } from '@/router'
-import store from '@/store'
+import {
+  constantRouterMap
+} from '@/router'
 import Layout from '@/components/common/Home.vue'
+import lazyLoading from '@/utils/lazyLoading'
 
-function getAddrouter(routers,permissions){
+//import commuip from 'commuip/index.js'
+//import commuam from 'commuam/index.js'
+
+function getAddrouter(routers, permissions) {
   let children = []
-  for( let i=0;i<permissions.length;i++){
-      let menu ={}
-      if (permissions[i].children && permissions[i].children.length) {
-        menu.component = Layout
-        menu.path = '/'
-        menu.children= getAddrouter(routers,permissions[i].children)
-        routers.push(menu)
-      }else{
-        menu.path= permissions[i].url
-        menu.meta={}
-        menu.meta.auth= true
-        menu.meta.title=permissions[i].name
-        // menu.component = function(){return import(`@/view${permissions[i].path}.vue`) }
-        // menu.component = function (resolve) {require(["@/view" + permissions[i].path + ".vue"], resolve)}
-        menu.component = function(resolve){require([permissions[i].path+".vue"], resolve)}
-        children.push(menu)
-      }
+  for (let i = 0; i < permissions.length; i++) {
+    let menu = {}
+    if (permissions[i].children && permissions[i].children.length) {
+      menu.component = Layout
+      menu.path = '/'
+      menu.children = getAddrouter(routers, permissions[i].children)
+      routers.push(menu)
+    } else {
+      menu.path = permissions[i].url
+      menu.meta = {}
+      menu.meta.auth = true
+      menu.meta.title = permissions[i].name
+      // menu.component = function(){return import(`@/view${permissions[i].path}.vue`) }
+      // menu.component = function(resolve){require([permissions[i].path+".vue"], resolve)
+      menu.component = lazyLoading(permissions[i].path)
+
+      // if (commuip[permissions[i].path]) {
+      //   menu.component = commuip[permissions[i].path]
+      // } else if (commuam[permissions[i].path]) {
+      //   menu.component = commuam[permissions[i].path]
+      // } else {
+      //   menu.component = lazyLoading(permissions[i].path)
+      // }
+      children.push(menu)
+    }
   }
-   return children;
+  return children;
 }
 
 const permission = {
@@ -37,11 +50,15 @@ const permission = {
     }
   },
   actions: {
-    GenerateRoutes({ commit }, data) {
+    GenerateRoutes({
+      commit
+    }, data) {
       return new Promise(resolve => {
-        let { permissions } = data;
-        let accessedRouters=[]
-        getAddrouter(accessedRouters,permissions)
+        let {
+          permissions
+        } = data;
+        let accessedRouters = []
+        getAddrouter(accessedRouters, permissions)
         commit("SET_ROUTERS", accessedRouters);
         resolve();
       });
