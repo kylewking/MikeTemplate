c47f97f5ce3f46852e60a556bf685df864ec7bd7
Kyle.wang <1191702694@qq.com>
2018-07-27 11:55:42 +0800

    commit

---
+++ meta.js
@@ -0,0 +1,41 @@
+const pkg = require('./package.json')
+const templateVersion = pkg.version
+module.exports = {
+
+    helpers: {
+        if_or(v1, v2, options) {
+
+            if (v1 || v2) {
+                return options.fn(this)
+            }
+
+            return options.inverse(this)
+        },
+        template_version() {
+            return templateVersion
+        },
+    },
+
+    prompts: {
+        name: {
+
+            type: 'string',
+            required: true,
+            message: 'Project name',
+        },
+        description: {
+            type: 'string',
+            required: false,
+            message: 'Project description',
+            default: 'A Vue.js project',
+        },
+        author: {
+            type: 'string',
+            message: 'Author',
+        }
+    },
+    filters: {
+
+    },
+    "completeMessage": "To get started:\n\n  {{^inPlace}}cd {{destDirName}}\n  {{/inPlace}}npm install\n  npm run dev\n\nDocumentation can be found at https://vuejs-templates.github.io/webpack"
+}
